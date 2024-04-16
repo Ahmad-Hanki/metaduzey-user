b7d0f528f0dfbfffa8c1f54fcc24c065cd63c519
ahmad <itxti909@gmail.com>
2024-04-16 19:49:32 +0300

    nlp page

---
+++ src/app/(static)/nlp/page.tsx
@@ -0,0 +1,12 @@
+import Nlp from '@/components/Nlp'
+import React from 'react'
+
+const NlpPgae = () => {
+  return (
+    <div>
+        <Nlp />
+    </div>
+  )
+}
+
+export default NlpPgae
---
+++ src/components/Nlp.tsx
@@ -0,0 +1,304 @@
+import Container from "./ui/Container";
+import Hero from "./ui/Hero";
+
+const Nlp = () => {
+  return (
+    <div className="bg-slate-200">
+      <Hero title="NLP" path="AnaSayfa => nlp" />
+      <Container>
+        <div className="flex gap-5 flex-col items-center text-center md:text-start md:items-start text-black/60">
+          <h1 className="text-red-700">Nlp</h1>
+
+          <div className="flex flex-col gap-3">
+            <h1 className="text-red-700">Çocuk Eğitiminde NLP </h1>
+            <div>
+              <p>
+                - NLP, çocuk ile veli arasında yapıcı bir iletişim köprüsünün
+                inşa edilebilmesi için gerekli temel malzemeler sunar.
+              </p>
+              <p>
+                - Kendinizi açık bir şekilde ifade edip, çocuğunuza da sizi daha
+                iyi anlayıp tepki göstermesini sağlayacak olan olumlu bir
+                iletişim kurmayı keşfedin. Örnek; çocuğunuza YAPMAMASI gereken
+                bir şeyi söyleyin. Mesela, "üstünü kirletmeden yemeğini ye !".
+                Muhtemelen bu söylediğiniz, istenmeyen davranışın meydana gelme
+                olasılığını artıracaktır, çünkü çocuk bu isteği yerine
+                getirebilmek için önce olumsuz yani istenmeyen davranış üzerinde
+                odaklanmak zorundadır. Bu yetmiyormuş gibi bir de tehdit edici
+                bir ses tonu kullandıysanız, çocukta meydana gelen korku ve
+                endişe dolu ruh hali neticesinde yemeğini üstünü batırmadan
+                yemesi gerçekçi bir beklenti dahi olamaz.
+              </p>
+              <p>
+                - Bu ve benzeri alanlarda çocuklarınızla hedef odaklı, etkili,
+                yapıcı ve uyumlu iletişim kurmayı öğrenin.
+              </p>
+            </div>
+          </div>
+
+          <div className="flex flex-col gap-3">
+            <h1 className="text-red-700">İş Dünyasında NLP</h1>
+            <div>
+              <p>
+                - İş dünyasında temel dinamiklerin oluşumu, sağlıklı iletişim
+                süreçlerinin gelişimine bağlıdır. Karşılıklı iletişim, etkileşim
+                ve güven ortamının oluşturulması, hedeflerin iyi düşünülmüş,
+                açık ve ekolojik yani sistem ile uyum içerisinde olması ve
+                esnekliğin geliştirilmesi gibi alanlarda NLP stratejik
+                yaklaşımlar sunar.
+              </p>
+              <p>
+                - İş dünyasında her geçen gün giderek önem kazanan bu
+                yaklaşımlar, günümüzde insan kaynakları uzmanları ve şirket içi
+                eğitim personeline teknik ve stratejik destek sağlayan
+                vazgeçilmez bir repertuar haline geldi.
+              </p>
+              <p>
+                - Herkesin öznel dünyası farklıdır. Farklı uzmanlık alanı,
+                kültür ya da tecrübelerin bir araya geldiği iş ortamlarında bu
+                mozaiğin yapı taşları daha da çeşitlilik kazanıyor.
+              </p>
+              <p>
+                - Burada bir araya gelen insanların etkileşiminden bir bütünün
+                oluşması ve tüm farklılıklara rağmen, çalışanların huzurlu bir
+                iş ortamı içerisinde ve ortak amaçlar doğrultusunda verimli bir
+                alışveriş gerçekleştirilebilmeleri, organizasyonun yapısı ve
+                şirket yönetiminin kararlarına bağlı olduğu kadar çalışanlarında
+                iç-dış iletişim kurma becerileriyle de yakından ilgilidir.
+              </p>
+              <p>
+                - İnsan kaynakları yönetimi, günümüzde tüm modern
+                organizasyonlar için vazgeçilmez bir öneme sahip. Birçok
+                yönetici, kurumsal danışman, ve insan kaynakları yöneticisi NLP
+                araçlarından faydalanarak, kurumsal yapılanma, takım oluşturma,
+                kalite yönetimi ve vizyon yaratma gibi alanlardaki etkinliğini
+                geliştiriyor.
+              </p>
+              <p>
+                - Online ve telefon müşteri hizmetleri, satış ve pazarlama
+                departmanları ve şikâyet yönetimi alanlarında da NLP her geçen
+                gün daha yoğun kullanım alanı bulmakta.
+              </p>
+            </div>
+          </div>
+
+          <div className="flex flex-col gap-3">
+            <h1 className="text-red-700">Eğitimde NLP</h1>
+            <div>
+              <span>
+                {" "}
+                {` {Daha hızlı nasıl öğrenir, öğretir ve bir profesyonel gibi eğitirsiniz?}`}
+              </span>
+              <p>
+                - Okuduğunuz, duyduğunuz veya gördüğünüz her şeyi hatırlama ve
+                anımsama yeteneğinizi önemli ölçüde artıran yeni bir öğrenme
+                biçimi
+              </p>
+              <p>
+                - Nasıl daha hızlı öğrenilir - böylece aynı kitabı yeniden
+                okuyarak, aynı ses kaydını yeniden dinleyerek veya notlarınızı
+                yeniden gözden geçirmek zorunda kalmayarak zamanınızı boşa
+                harcamazsınız.
+              </p>
+              <p>
+                - Ders olarak çalıştığınız her şeyi %100 daha çok hatırlamanıza
+                yardımcı olacak özel bir öğrenme tekniği - Böylece bilgiyi
+                öğrendikten sonra onu daha iyi şekilde “Beyninizde
+                canlandırabilirsiniz. “
+              </p>
+              <p>
+                - Öğrencilerinizin HIZLI öğrenmesinin garanti edildiği nasıl bir
+                öğretme stili.
+              </p>
+              <p>
+                - NLP'nin temel amaçlarından biri de, tam öğrenmenin nasıl
+                gerçekleştiğini ve yeni bilginin daha önce var olan bilgiler ile
+                etkileşimini anlamak. Bu alandaki araştırmalar sayesinde,
+                günümüzde birçok etkili öğrenme ve öğretme stratejisi
+                geliştirilebilmiştir.
+              </p>
+              <p>
+                - NLP teknikleri, velilere, antrenörler ya da eğitmenlere,
+                çocuklarının ya da öğrencilerinin daha başarılı sonuçlar
+                almalarını sağlayacak olan metot ve uygulamalar sunar.
+              </p>
+              <p>
+                - Bundan dolayı eğitim öğretim alanında çalışan birçok öğretim
+                görevlisi bazı temel NLP aracını, ders metodolojisine ve sınıf
+                yönetimi alanındaki çalışmalarına entegre etmiştir.
+              </p>
+              <p>
+                - NLP, gruplarla birlikte çalışan insanların, mesleklerini daha
+                başarılı uygulayabilmelerini sağlayacak olan araçlar sunar.
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-col gap-3">
+            <h1 className="text-red-700">Kişisel Gelişimde NLP</h1>
+            <div>
+              <p>
+                - Birçok insan NLP' i kendi kişisel gelişimi için kullanıyor.
+                Sizi kısıtlayan korkuların, alışkanlıkların ve benzeri
+                etkenlerin üstesinden gelip, sizi hedefleriniz doğrultusunda
+                destekleyen yeni davranış seçeneklerinin oluşturulmasında NLP
+                size destek sağlar.
+              </p>
+              <p>
+                - NLP' nin temel varsayımları ve özel uygulamaları, geçmişten
+                gelen korkularınızı, kısıtlayıcı inançlarınızı, zarar veren
+                alışkanlıklarınızı değiştirmede size yardımcı olacak etkili
+                çözümler sunarak yaşam enerjinizi yeniden keşfetmenizi sağlar.
+              </p>
+              <p>
+                - NLP bu süreci, iletişim kurma, tepki verme ve hissetme
+                seçeneklerinizi zenginleştirerek destekler. Daha fazla seçeneğe
+                sahip insanlar, daha isabetli tercihlerde bulunabilirler.
+              </p>
+              <p>
+                - Yaşamınızdan memnun değilseniz, NLP sizlere belki de daha önce
+                üzerinde hiç düşünmediğiniz yeni yaşam alternatifleri sunabilir.
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-col gap-3">
+            <h1 className="text-red-700">
+              Psikoterapi ve Danışmanlık Hizmetlerinde NLP
+            </h1>
+            <div>
+              <p>
+                - Geçtiğimiz 30 sene içerisinde NLP özellikle psiko - sosyal
+                danışmanlık ve terapi hizmeti veren kurumlarda her geçen gün
+                daha da yaygınlaşan uygulama alanları bulmaya başladı.
+              </p>
+              <p>
+                - Sağlık alanında NLP kullanımın bu kadar yaygınlaşmasının
+                sebebi, NLP araştırmalarının büyük bir bölümünün, hastalarına
+                kendi istekleri doğrultusunda değişmelerine etkili biçimde
+                yardım edebilen hekim ve psikoterapi uzmanların yöntemleri
+                üzerinde yoğunlaşmasından kaynaklanmaktadır.
+              </p>
+              <p>
+                - Bu araştırmalar başlamadan önce, başarılı hekim ve psikoterapi
+                uzmanlarının kullanmış oldukları etkili dil kalıpları, teknikler
+                ve yöntemler bütünü bilinçdışı bir yeterlilik ya da yetenek
+                olmaktan öteye gidemezken, günümüzde NLP araştırmaları ve
+                modelleme çalışmaları sayesinde bu kalıplar milyonlarca insana
+                ulaştırılabildi.
+              </p>
+              <p>
+                - NLP, bünyesinde kişisel gelişim ve değişim süreçlerinde
+                kullanılan ve kalıcı sonuçlar sağlayan birçok teknik ve yöntem
+                ihtiva eder.
+              </p>
+              <p>
+                - Bu yöntemlerden belki de en tanınmış olanlarını, kişisel korku
+                ve fobilerden kurtulmada kullanılan 'NLP Fobi Tekniği' dir.
+                Senelerdir süregelen ve belki de yaşamınızı sürekli kısıtlayan
+                (ör. yükseklik yada uçak korkusu) ve benzeri fobilerden çok kısa
+                süre içerisinde kurtulabilirsiniz.
+              </p>
+              <p>
+                - Benzeri klinik vakalarda da NLP, kişiye özgü dil kalıpları ve
+                uygulamalar kullanır. Tüm süreçlerde birey bir bütün olarak ele
+                alındığı gibi karmaşık ve dinamik başka sistemlerinde bir
+                parçası olarak değerlendirilir ve buna uygun sistematik, itinalı
+                yaklaşım sonucunda çözümün kişinin içerisinde bulunduğu, aile,
+                iş, inançlar, değerler ve benzeri karmaşık sistemlerle de uyum
+                içerisinde olması amaçlanır.
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-col gap-3">
+            <h1 className="text-red-700">NLP ile Coaching</h1>
+            <div>
+              <p>
+                - İş dünyası, endüstri, yönetim ve psikososyal alanlarda
+                Coaching'e duyulan gereksinim her geçen gün artmakta. Birçok
+                profesyonel Coach, kişi, grup ya da daha büyük organizasyonları,
+                planlama, kritik dönemleri atlatma, kişisel performansı açığa
+                çıkarma ya da hedeflere ulaşma süreçlerinde destekleme amacıyla
+                vermiş olduğu bu özel danışmanlık hizmetinde sık sık NLP
+                yöntemlerine başvurur.
+              </p>
+              <p>
+                - Coaching seanslarında, iyi belirlenmiş hedeflere ulaşma
+                sürecinde, çözüm odaklı stratejiler geliştirilir ve gerek
+                duyulan kaynakların gelişimi sağlanır.
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-col gap-3">
+            <h1 className="text-red-700">Sağlık Hizmetlerinde NLP</h1>
+            <div>
+              <p>
+                - NLP' nin sunmuş olduğu temel varsayımlar, iletişim kalıpları,
+                yöntem ve uygulamaları doktor, hekim, sağlık personeli ve kendi
+                sağlığı ile yakından ilgilenen kişilere faydalı içerikler sunar.
+              </p>
+              <p>
+                - Sağlık sektörü hızla gelişmekte. Bu alanda çalışan personelin
+                de, tıbbi ya da bakıma yönelik bilgi ve becerilerinin dışında,
+                gelişmeyle birlikte oluşan kör noktaları aydınlatabilecek özel
+                bilgi ve becerilere de sahip olmaları her geçen gün daha büyük
+                önem kazanıyor.
+              </p>
+              <p>
+                - Teknolojinin gelişimi ve şehirleşme ile birlikte sağlık
+                hizmetlerinin de bir nevi makineleştiği ya da ruhsuzlaştığı
+                yönünde artan eleştiriler, insana özel ilginin yerini, nerdeyse
+                kişisel hiçbir temasın gerçekleşmediği soğuk bir etkileşime
+                bırakması ve alternatif tıbba yönelen insanların artışı, bu
+                alanda meydana gelen boşluğun ve alınması gereken önemlerin
+                habercisidir.
+              </p>
+              <p>
+                - İş ilanlarına bakıldığında bu alanlarda göreve alınacak
+                personelden, empati, etkili sunum, müzakere ve benzeri yüksek
+                düzeyde sosyal becerileri beraberinde getirmeleri beklenmekte.
+                Hastalığın tedavi sürecinin ne yönde gelişeceğinin, hasta ile
+                hekim arasında meydana gelen iletişim ve bunun sonucunda oluşan
+                güven ortamıyla da yakından ilişkili olduğu birçok bilimsel
+                araştırma tarafından ispatlanmıştır.
+              </p>
+              <p>
+                - Bundan dolayı, NLP sağlık alanında görevli personele de,
+                hastalarıyla veya görevliler ile kooperatif ve uyumlu bir ilişki
+                içerisine girmeyi kolaylaştıracak ve hastalıkların da tedavi
+                sürecini olumlu yönde destekleyecek olan araçlar sunar.
+              </p>
+              <p>
+                - Geçmiş yıllarda, NLP özellikle kişisel sağlığın nasıl
+                desteklenebileceği, bağışıklık sisteminin desteklenmesi, derin
+                inanç ve hastalıklar arasındaki ilişkiler, travmatoloji ve
+                sağlıkla ilgili benzeri alanlarda araştırmalarını
+                yoğunlaştırmıştır.
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-col gap-3">
+            <h1 className="text-red-700">
+              Sıra dışı düşünme becerileri ve NLP
+            </h1>
+            <div>
+              <p>
+                - Birçok sanatçı, yazar ve yorumcu, almış oldukları NLP
+                eğitimlerini, sıra dışı düşünce ruhunu kamçılayan ya da
+                engelleyen birçok etmenin açığa çıkmasına sağladığından, çok
+                değerli buluyorlar.
+              </p>
+              <p>
+                - Bu alanda tanınan NLP uygulamalarından bir tanesi Robert Dilts
+                tarafından Amerika’da geliştirilen Disney- Stratejisidir. Bu ve
+                benzeri uygulamalar, belki de eğitim ve şartlanma sürecinizde
+                körelmiş olan yaratıcı kaynaklarınıza yeniden keşfedip yaşama
+                yansıtmanızı destekleyecek.
+              </p>
+            </div>
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
+};
+
+export default Nlp;
